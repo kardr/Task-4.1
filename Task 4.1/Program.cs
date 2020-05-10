fc28b327ea36245870418b6f1c1c6b48f8629fc1
Карина Дроздова <K.Drozd33@yandex.ru>
2020-05-10 19:00:27 +0300

    .

--- Task 4.1/Program.cs
+++ Task 4.1/Program.cs
@@ -8,8 +8,18 @@ namespace Task_4._1
 {
     class Program
     {
+        private static double Rec(double i, double n)
+        {
+            return i < n ? Math.Sqrt(i + Rec(++i, n)) : Math.Sqrt(i);
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите N: ");
+            var n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Результат: " + n / Rec(1, n));
+            Console.ReadLine();
         }
     }
 }
+
